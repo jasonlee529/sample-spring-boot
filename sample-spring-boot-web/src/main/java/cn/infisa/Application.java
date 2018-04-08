5eb4f115ac6d40a3193134df78c01c1dcc584d59
jasonlee529 <libo529@gmail.com>
2018-04-08 23:04:56 +0800

    remove spring-data-jpa-starter

--- sample-spring-boot-web/src/main/java/cn/infisa/Application.java
+++ sample-spring-boot-web/src/main/java/cn/infisa/Application.java
@@ -1,5 +1,8 @@
+package cn.infisa;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -7,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import java.util.Arrays;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class Application {
 
     public static void main(String[] args) {
--- sample-spring-boot-web/src/main/java/cn/infisa/web/HelloController.java
+++ sample-spring-boot-web/src/main/java/cn/infisa/web/HelloController.java
@@ -1,4 +1,4 @@
-package web;
+package cn.infisa.web;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
