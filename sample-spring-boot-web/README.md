7c8f69e921e831bf7e469b6a3cabfe5f13138b84
jason <libo529@gmail.com>
2018-04-11 10:18:50 +0800

    update readme

---
+++ sample-spring-boot-web/README.md
@@ -0,0 +1,17 @@
+## 运行环境
+java > 1.8
+maven > 3.0
+
+## 运行方式
+### 1.打包后发布
+```jshelllanguage
+$ mvn clean package && java -jar target/spring-boot-web-1.0-SNAPSHOT.jar
+```
+
+### 2.maven直接运行
+```jshelllanguage
+$ mvn spring-boot:run
+```
+
+### 3.java application方式执行
+Application.java 右键 选择'run Application'
--- sample-spring-boot-web/src/main/java/cn/infisa/Application.java
+++ sample-spring-boot-web/src/main/java/cn/infisa/Application.java
@@ -4,12 +4,15 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 import java.util.Arrays;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class Application {
 
     public static void main(String[] args) {
