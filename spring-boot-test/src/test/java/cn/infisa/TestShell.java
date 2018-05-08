e817a45210959e586605c7c006f607eb6e8db6c3
jason <libo529@gmail.com>
2018-05-08 18:00:37 +0800

    update

--- spring-boot-test/src/test/java/cn/infisa/TestShell.java
+++ spring-boot-test/src/test/java/cn/infisa/TestShell.java
@@ -2,19 +2,27 @@ package cn.infisa;
 
 import org.junit.Test;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.LineNumberReader;
+import java.io.*;
 import java.util.concurrent.Executors;
 
 public class TestShell {
 
     @Test
-    public void testShell() {
-        String cmd="python3 /opt/danxi/daniDeal.py";
-        System.out.println(exec(cmd));
-//        executeCommand("ping -c 3 www.baidu.com");
+    public void testOne() throws IOException, InterruptedException {
+        Process exec = Runtime.getRuntime().exec(new String[] { "uname" ,"-a"});
+        exec.waitFor();
+        BufferedReader reader =
+                new BufferedReader(new InputStreamReader(exec.getInputStream()));
+        System.out.println(exec.exitValue());
+        System.out.println(reader.readLine());
     }
+    @Test
+    public void testShell() {
+        String cmd = "python3 /opt/danxi/daniDeal.py";
+        System.out.println(exec(cmd));
+        executeCommand("ping -c 3 www.baidu.com");
+    }
+
     private String executeCommand(String command) {
 
         StringBuffer output = new StringBuffer();
@@ -23,12 +31,12 @@ public class TestShell {
         try {
             p = Runtime.getRuntime().exec(command);
             int exitCode = p.waitFor();
-
+            System.out.println(exitCode);
             BufferedReader reader =
                     new BufferedReader(new InputStreamReader(p.getInputStream()));
 
             String line = "";
-            while ((line = reader.readLine())!= null) {
+            while ((line = reader.readLine()) != null) {
                 output.append(line + "\n");
             }
 
@@ -43,17 +51,18 @@ public class TestShell {
     public static Object exec(String cmd) {
         StringBuffer output = new StringBuffer();
         try {
-            String[] cmdA = { "/bin/sh", "-c", cmd };
+            String[] cmdA = {"/bin/sh", "-c", cmd};
             Process process = Runtime.getRuntime().exec(cmdA);
             int exitCode = process.waitFor();// 2 有异常，０　执行完成
-            System.out.println(exitCode);
-            System.out.println(process.getErrorStream());
-            System.out.println(process.getOutputStream());
-            BufferedReader reader =
-                    new BufferedReader(new InputStreamReader(process.getErrorStream()));
-
+            InputStream is = null;
+            if (exitCode == 0) {
+                is = process.getInputStream();
+            } else {
+                is = process.getErrorStream();
+            }
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
             String line = "";
-            while ((line = reader.readLine())!= null) {
+            while ((line = reader.readLine()) != null) {
                 System.out.println(line);
                 output.append(line + "\n");
             }
