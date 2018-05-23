16700c38aa51add6fe50faa062f5df8f00fda14d
jason <libo529@gmail.com>
2018-05-23 09:36:17 +0800

    update

---
+++ spring-boot-data-rest/src/main/java/cn/infisa/repository/PersonRepository.java
@@ -0,0 +1,10 @@
+package cn.infisa.repository;
+
+import cn.infisa.entity.Person;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+public interface PersonRepository extends PagingAndSortingRepository<Person, Long>, JpaSpecificationExecutor<Person> {
+}
