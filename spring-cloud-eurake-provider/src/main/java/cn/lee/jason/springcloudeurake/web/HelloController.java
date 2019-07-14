262fd935cd6e9a97dafc56940bdd9df6dfbf68a3
jason <libo529@gmail.com>
2019-07-14 16:52:19 +0800

    add config

--- spring-cloud-eurake-provider/src/main/java/cn/lee/jason/springcloudeurake/web/HelloController.java
+++ spring-cloud-eurake-provider/src/main/java/cn/lee/jason/springcloudeurake/web/HelloController.java
@@ -5,10 +5,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -19,10 +21,20 @@ public class HelloController {
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String index() {
-        List<String> ids = client.getServices();
-        ServiceInstance instance = client.getInstances(ids.get(0)).get(0);
-        logger.info("/hello,host:" + instance.getHost() + ",service_id:" + instance.getServiceId());
         return "Hello World";
+    }
+
+    @GetMapping(value = "/info")
+    public List<String> info() {
+        List<String> result = new ArrayList<>();
+        List<String> ids = client.getServices();
+        for (String id : ids) {
+            List<ServiceInstance> instances = client.getInstances(id);
+            for(ServiceInstance instance : instances){
+                result.add("/info,URL : " + instance.getUri() + ",service_id : " + instance.getServiceId());
+            }
+        }
+        return result;
     }
 }
 
