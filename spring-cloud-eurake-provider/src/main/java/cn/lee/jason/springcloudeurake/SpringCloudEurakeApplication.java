8092b71a0f33cb1db422649d4f9ecfb8d0a21180
jason <libo529@gmail.com>
2019-07-14 23:44:52 +0800

    change config

--- spring-cloud-eurake-provider/src/main/java/cn/lee/jason/springcloudeurake/SpringCloudEurakeApplication.java
+++ spring-cloud-eurake-provider/src/main/java/cn/lee/jason/springcloudeurake/SpringCloudEurakeApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
-@EnableEurekaClient
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@EnableDiscoveryClient
 public class SpringCloudEurakeApplication {
 
     public static void main(String[] args) {
