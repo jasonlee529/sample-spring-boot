0680bde5c39c81dd243f61b652dc9121bc9ad96f
jason <libo529@gmail.com>
2019-08-02 00:14:50 +0800

    add hystrix

--- spring-cloud-zuul/spring-cloud-eurake-consumer/src/main/java/cn/lee/jason/springcloudeurake/SpringCloudEurakeApplication.java
+++ spring-cloud-zuul/spring-cloud-eurake-consumer/src/main/java/cn/lee/jason/springcloudeurake/SpringCloudEurakeApplication.java
@@ -8,12 +8,14 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 @EnableDiscoveryClient
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableFeignClients
+@EnableHystrix
 public class SpringCloudEurakeApplication {
     @Bean
     @LoadBalanced
